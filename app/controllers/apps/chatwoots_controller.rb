5dc5c8cb2c5a09e436731bf2d21ab71e79e98633
Yukio <45243355+YukioArie@users.noreply.github.com>
2024-12-30 06:06:53 -0300

    Fix embedding chatwoot authentication flow (#445)
    * Fix embedding chatwoot authentication flow
    
    * Remove whitelabel concern

--- app/controllers/apps/chatwoots_controller.rb
+++ app/controllers/apps/chatwoots_controller.rb
@@ -1,15 +1,16 @@
 class Apps::ChatwootsController < ActionController::Base
-  before_action :load_chatwoot, execpt: :webhooks
-  before_action :authenticate_by_token, if: -> { current_user.blank? }
-  skip_before_action :verify_authenticity_token, execpt: :embedding
+  before_action :load_chatwoot
+  before_action :authenticate_by_token, if: :check_user_authentication
+  skip_before_action :verify_authenticity_token, except: :embedding
   layout 'embed'
 
   def webhooks
-    Accounts::Apps::Chatwoots::Webhooks::ProcessWebhookJob.perform_later(params.to_json, params['token'])
+    Accounts::Apps::Chatwoots::Webhooks::ProcessWebhookJob.perform_later(params.to_json, @chatwoot.account_id)
     render json: { ok: true }, status: 200
   end
 
-  def embedding; end
+  def embedding
+  end
 
   def embedding_init_authenticate
     @token = params['token']
@@ -18,14 +19,19 @@ class Apps::ChatwootsController < ActionController::Base
   def embedding_authenticate
     event = JSON.parse(params['event'])
     @user_email = event['data']['currentAgent']['email']
-    user = User.find_by(email: @user_email)
+    user = User.find_by(email: @user_email, account_id: @chatwoot.account_id)
     return render 'user_not_found', status: 400 if user.blank?
 
+    sign_out_all_scopes
     sign_in(user)
-    redirect_to embedding_apps_chatwoots_path
+    redirect_to embedding_apps_chatwoots_path(token: params['token'])
   end
 
   private
+
+  def check_user_authentication
+    User.find_by_id(current_user&.id).blank?
+  end
 
   def authenticate_by_token
     if @chatwoot.present? && action_name == 'embedding'
