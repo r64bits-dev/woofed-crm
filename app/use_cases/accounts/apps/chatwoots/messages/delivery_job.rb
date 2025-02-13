b509708f9eca93522f434dd4087f653d6544cae0
danielmachado <danielmachadopintos@gmail.com>
2025-02-13 11:23:19 -0300

    feat: add more logs

--- app/use_cases/accounts/apps/chatwoots/messages/delivery_job.rb
+++ app/use_cases/accounts/apps/chatwoots/messages/delivery_job.rb
@@ -2,22 +2,23 @@ class Accounts::Apps::Chatwoots::Messages::DeliveryJob < ApplicationJob
   self.queue_adapter = :good_job
   def perform(event_id)
     event = Event.find(event_id)
-    if event.should_delivery_event_scheduled?
-      result = Accounts::Apps::Chatwoots::GetConversationAndSendMessage.call(
-        event.app,
-        event.contact.additional_attributes['chatwoot_id'],
-        event.additional_attributes['chatwoot_inbox_id'],
-        event
-      )
-      if result.key?(:ok)
-        event.additional_attributes['chatwoot_id'] = result[:ok]['id']
-        event.additional_attributes['chatwoot_conversation_id'] = result[:ok]['conversation_id']
-        event.done = true
-        event.save!
-        { ok: event }
-      else
-        { error: result[:error] }
-      end
+    p 'delivery job', event
+    return unless event.should_delivery_event_scheduled?
+
+    result = Accounts::Apps::Chatwoots::GetConversationAndSendMessage.call(
+      event.app,
+      event.contact.additional_attributes['chatwoot_id'],
+      event.additional_attributes['chatwoot_inbox_id'],
+      event
+    )
+    if result.key?(:ok)
+      event.additional_attributes['chatwoot_id'] = result[:ok]['id']
+      event.additional_attributes['chatwoot_conversation_id'] = result[:ok]['conversation_id']
+      event.done = true
+      event.save!
+      { ok: event }
+    else
+      { error: result[:error] }
     end
   end
 end
