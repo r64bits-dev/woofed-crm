57c04f8cb3929a5f02c55655360b505f8ea8afd4
danielmachado <danielmachadopintos@gmail.com>
2025-02-13 10:20:08 -0300

    feat: add more logs

--- app/use_cases/accounts/apps/chatwoots/webhooks/events/message.rb
+++ app/use_cases/accounts/apps/chatwoots/webhooks/events/message.rb
@@ -1,8 +1,10 @@
 class Accounts::Apps::Chatwoots::Webhooks::Events::Message
-
   def self.call(chatwoot, webhook)
-    contact = Accounts::Apps::Chatwoots::Webhooks::ImportContact.call(chatwoot, webhook['conversation']['contact_inbox']['contact_id'])[:ok]
+    contact = Accounts::Apps::Chatwoots::Webhooks::ImportContact.call(chatwoot,
+                                                                      webhook['conversation']['contact_inbox']['contact_id'])[:ok]
+    p 'contact', contact
     message = Accounts::Apps::Chatwoots::Webhooks::ImportMessage.new(chatwoot, contact, webhook).call
-    return { ok: message }
+    p 'message', message
+    { ok: message }
   end
 end
